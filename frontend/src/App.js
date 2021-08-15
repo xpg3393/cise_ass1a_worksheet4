1aeda877a0690fd25b4a28362d9cc1f48441e466
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-08-16 00:14:30 +1200

    finally compiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import{
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Redirect
 } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -24,6 +25,8 @@ const App = () =>  {
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404" />
           </div>
         </div>
         </Router>
