778ce033c017c5801211d14139166f19b667b21a
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-08-16 00:03:40 +1200

    Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+            <ul className="header">
+                <li><a href = "/">Home</a></li>
+                <li><a href = "/SEPractice">Select the Practice</a></li>
+                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+            </ul>
+          <div className="content">
+            <
+          </div>
+        </div>
+        </Router>
+    );
 }
-
+ 
 export default App;
